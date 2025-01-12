996dcb0f5bfe3bde9705f33dd4aa0b96c7d11de3
dev-himaxu <himanshu983983@gmail.com>
2025-01-12 08:16:25 +0530

    Some Change in Leap year code;

--- LogicBuilding/src/leapYearCheck/Leapyear.java
+++ LogicBuilding/src/leapYearCheck/Leapyear.java
@@ -4,19 +4,14 @@ package leapYearCheck;
 
 public class Leapyear {
 	public static void main(String[] args) {
-		int year = 2024;
-		if (year % 4 == 0) {
-			if (year % 100 == 0) {
-				if (year % 400 == 0) {
-					System.out.println(year + " is a Century Leap Year");
-				} else {
-					System.out.println(year + " is not a Leap Year");
-				}
-			} else {
-				System.out.println(year + " is a Leap Year");
-			}
+		int year = 1000;
+		if (year % 400 == 0) {
+			System.out.println(year + " is a Century Leap Year");
+		} else if (year % 4 == 0 && year % 100 != 0) {
+			System.out.println(year + " is Leap Year");
 		} else {
 			System.out.println(year + " is not a Leap Year");
 		}
+
 	}
 }
