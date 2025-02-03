6c090ab9f9d07af0eb2cea4cda89f33d575748b7
Dev-Himaxu <himanshu983983@gmail.com>
2025-02-03 20:58:56 +0530

    Update UpperAndLowerCase.java

--- LogicBuilding/src/upperCaseOrLowerCase/UpperAndLowerCase.java
+++ LogicBuilding/src/upperCaseOrLowerCase/UpperAndLowerCase.java
@@ -10,7 +10,7 @@ public class UpperAndLowerCase {
 		for (int i = 0; i < ch.length; i++) {
 			if (ch[i] >= 'a' && ch[i] <= 'z') {
 				ch[i] = (char) (ch[i] - 32);
-			} else if (ch[i] >= 'A' && ch[i] < 'Z') {
+			} else if (ch[i] >= 'A' && ch[i] <= 'Z') {
 				ch[i] = (char) (ch[i] + 32);
 			}
 		}
